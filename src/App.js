f0f585122212c5e46ff958f9a51b74cbcadb04e7
preethib20 <145477588+preethib20@users.noreply.github.com>
2024-12-09 15:11:23 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignUpForm from './SignUpForm';
+
+const App = () => {
+  return (
+    <div>
+      <h3>User Sign-Up</h3>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default App;
